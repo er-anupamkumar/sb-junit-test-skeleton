f5fa8cbde7e04fb0fb4fe3a831b396144de86f4d
anupamkumar <vndkumar.anupam@bcg.com>
2024-09-03 21:54:28 +0200

    chore(setup) initialize the repo

---
+++ src/main/java/com/anupam/sb_junit_test_skeleton/SbJunitTestSkeletonApplication.java
@@ -0,0 +1,13 @@
+package com.anupam.sb_junit_test_skeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJunitTestSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJunitTestSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anupam/sb_junit_test_skeleton/SbJunitTestSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupam.sb_junit_test_skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJunitTestSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
